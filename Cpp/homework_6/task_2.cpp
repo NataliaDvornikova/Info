a6dd2a5847df96a77bb7247df8a8c57141fd4ba2
NataliaDvornikova <112897561+NataliaDvornikova@users.noreply.github.com>
2023-10-19 00:24:34 +0300

    Create task_2.cpp

---
+++ Cpp/homework_6/task_2.cpp
@@ -0,0 +1,18 @@
+//Найти факториал заданного числа.
+#include <iostream>
+#include <locale>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a;
+    int b = 1;
+    cout << "Введите число: ";
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+        b = b * i;
+    cout << a << "! = " << b << endl;
+    return 0;
+}
